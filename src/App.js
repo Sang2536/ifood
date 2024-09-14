8081ec59a0a26cd4144a0c1c140a031a23dd1b72
Sang-2000 <dovansang2536@gmail.com>
2024-09-15 00:57:51 +0700

    change project structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
+import './scss/app.scss';
 
-import HomePage from "./PageLayouts/HomePage";
+import HomePage from './PageLayouts/HomePage';
 
 function App() {
-  return (
-    <div className="App">
-      <p>App (PageLayouts (PageComponents (SingleComponents))) </p>
-      
-      <HomePage />
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>App (PageLayouts (PageComponents (SingleComponents))) </p>
+
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
